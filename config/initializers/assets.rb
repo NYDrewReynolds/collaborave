e75d5e3772ce4fec0410fba26c61904563d80ceb
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-04 18:26:32 -0600

    Fix track scrolling issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules", "plangu
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( plangular-load.js )
