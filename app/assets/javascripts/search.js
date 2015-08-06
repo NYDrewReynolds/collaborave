5af33c241270faa43c8615422886fcce7b967fba
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-06 13:04:03 -0600

    Edits to search feature

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,16 +2,23 @@ jQuery(document).ready(function ($) {
 
     $("#search").keyup(function () {
         // get the search form value
-        var formData = $("#search").val();
+        var searchData = $("#search").val();
 
         // clear search results div
         $('#sc-results').html('');
 
-        // find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
-        SC.get('/tracks', {q: formData, duration: {from: 3000000}, limit: '40'}, function (tracks) {
+        SC.get('/tracks', {q: searchData, limit: '20'}, function (tracks) {
 
             for (var i = 0, len = tracks.length; i < len; i++) {
-                $('#sc-results').append('<div>' + '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + '</a>' + '</div><br>');
+                $('#sc-results').append(
+                    '<div>'
+                    + '<a href="'
+                    + tracks[i].permalink_url
+                    + '">'
+                    + tracks[i].title
+                    + '</a>'
+                    + '</div><br>'
+                );
 
                 var track_url = tracks[i].permalink_url;
                 var player = SC.oEmbed(track_url, {auto_play: true});
--- app/models/party.rb
+++ app/models/party.rb
@@ -2,4 +2,6 @@ class Party < ActiveRecord::Base
   belongs_to :user
   has_many :party_songs
   has_many :songs, through: :party_songs
+
+  validates :name, presence: true
 end
