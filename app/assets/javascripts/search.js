42734eee27f4aedd67a9679c29a34a982174e655
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-08 18:34:19 -0600

    Combine JS Search & Add Files. jQuery now recognizing search results

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -10,22 +10,47 @@ jQuery(document).ready(function ($) {
             for (var i = 0, len = tracks.length; i < len; i++) {
                 $('#sc-results').append(
                     '<br><div>'
-                    + '<a id="search-result" href="#" permalink_url="'
+                    + "<a class='search-result' href='#' data-url='"
+                        //+ "<a class='search-result' href='"
                     + tracks[i].permalink_url
-                    + '">'
+                    + "'>"
                     + tracks[i].title
-                    + '</a>'
-                    + '</div>'
+                    + "</a>"
+                    + "</div>"
                 );
-
-                var track_url = tracks[i].permalink_url;
-                var player = SC.oEmbed(track_url, {auto_play: true});
-
-                $('#sc-results').html(player);
+                //
+                //var track_url = tracks[i].permalink_url;
+                //var player = SC.oEmbed(track_url, {auto_play: true});
+                //
+                //$('#sc-results').html(player);
             }
-        });
 
-        return false;
+            $('a.search-result').on('click', function () {
+                console.log("WOOOWWOWOWOW!!!");
+                var songParams = {
+                    post: {permalink_url: $('permalink_url')}
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: '/api/v1/add_song',
+                data: songParams,
+                success: function (newSong) {
+                    $('#latest-posts').append(
+                        "<div class='sc-track' plangular='"
+                        + newSong.url
+                        + "'><div class='pull-left'<img ng-src='{{track.artwork_url}}'></div> <h3>{{track.user.username}} - {{track.title}}</h3> <button ng-click='playPause()'>Play/Pause</button><progress ng-click='seek($event)' ng-value='currentTime / duration' > {{ currentTime / duration }} </progress> <br> <a ng-href='"
+                        + newSong.url
+                        + "'> View on SoundCloud </a>"
+                        + "</div>"
+                    )
+                }
+            })
+        });
     });
 
+    return false;
 });
+
+})
+;
