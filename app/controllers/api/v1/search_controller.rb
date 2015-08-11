de9b8e493d08c25eb5c8afbf99c00b218275917a
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-11 11:24:07 -0600

    Working on song creation fix

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::SearchController < ApplicationController
   def add_song
     song_url = params[:post][:permalink_url]
     party_id = params[:post][:party_id]
-    song = Song.create(url: song_url)
-    # song = Song.create(url: song_url) if Song.find_by(url: song_url).nil?
+    # song = Song.create(url: song_url)
+    song = Song.create(url: song_url) if Song.find_by(url: song_url).nil?
     party = Party.find_by(id: party_id)
     party.party_songs.create(song: song)
 
